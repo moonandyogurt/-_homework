8b963db6dce57a6005916800c24e4b7e4d2e7fd9
Sanzhar Rakhmetolla <sanzarrahmetolla649@gmail.com>
2022-12-13 23:48:33 +0600

    first commit

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿void Zadacha25()
+{
+
+    Console.Write("enter number A->");
+    int numberA = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine();
+    Console.Write("enter number B->");
+    int numberB = Convert.ToInt32(Console.ReadLine());
+    
+    int sum = 1;
+    int counter = 1;
+    while(counter <= numberB)
+    {
+        sum = sum * numberA;
+        Console.Write($"{sum}*{numberA} = ");
+        
+        Console.WriteLine(sum);
+        counter++;
+        
+    }
+    Console.WriteLine("answer: "+ sum);
+}
+
+Zadacha25();
